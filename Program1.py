a9162545b0fc0f98a3aa4f23aa18c15e2c76a198
PrabhodaCS <prabhodacs@gmail.com>
2024-11-20 01:33:56 +0000

    Trial Feature Branch

---
+++ Program1.py
@@ -0,0 +1,12 @@
+"""
+/**
+ * @author Prabhoda CS
+ * @email pcs52@cam.ac.uk
+ * @create date 20-11-2024 01:25:37
+ * @modify date 20-11-2024 01:25:37
+ * @desc [description]
+ */
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import math
