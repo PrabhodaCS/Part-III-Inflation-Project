6b81b7d8e6a4ae36c0ec128618aa98d0e241f176
William Barker <barker@fzu.cz>
2025-06-01 12:30:37 +0200

    Added improved plot for Prabhoda.

--- GetDist/GetDist.py
+++ GetDist/GetDist.py
@@ -2,18 +2,19 @@ from getdist import plots   # Prabhoda: you should install getdist via cobaya
 from getdist.mcsamples import loadMCSamples 
 import matplotlib.pyplot as plt
 
-# Prabhoda: you don't have this but I'll give it to you.
-samples = loadMCSamples('/home/barker/Downloads/base_r/plikHM_TTTEEE_lowl_lowE_BK15_lensing/base_r_plikHM_TTTEEE_lowl_lowE_BK15_lensing')
+# Prabhoda: please explore the directory of chains and add extra combinations if you like
+base_r_BK15_lensing = loadMCSamples('base_r/plikHM_TTTEEE_lowl_lowE_BK15_lensing/base_r_plikHM_TTTEEE_lowl_lowE_BK15_lensing')
+base_r_lensing = loadMCSamples('base_r/plikHM_TTTEEE_lowl_lowE_lensing/base_r_plikHM_TTTEEE_lowl_lowE_lensing')
+base_r = loadMCSamples('base_r/plikHM_TTTEEE_lowl_lowE/base_r_plikHM_TTTEEE_lowl_lowE')
 
 # Grab the slices.
 plotter = plots.get_subplot_plotter()
-plotter.plot_2d(samples, 'ns', 'r', filled=True, legend_labels=['Planck 2018'])
+plotter.plot_2d([base_r,base_r_BK15_lensing], 'ns', 'r', filled=True,colors=['green', ('#F7BAA6', '#E03424')])  #   Prabhoda: make the colors nicer
+plotter.add_legend(['Planck','Planck+lensing+BICEP/Keck'])   # Prabhoda: make the legend more informative 
 
-# Prabhoda's fancy inflation model on top.
-plt.scatter([0.965], [0.005], marker='*', s=150, color='red', label='Sarjapur inflation')
-plt.legend()
+# Prabhoda: add your model here
+plt.scatter([0.965], [0.005], marker='*', s=150, color='red')
 plt.xlabel(r'$n_s$')
 plt.ylabel(r'$r$')
-plt.tight_layout()
-plt.gcf().set_size_inches(10, 8)
-plt.savefig('ns_r_plot.png', dpi=300)
+plt.gcf().set_size_inches(8, 8)
+plt.savefig('ns_r_plot.pdf', dpi=300)
